94e22256cc1c0247ef1be3858d47f81a6684ca4e
Fenghua <gaofenghua@gmail.com>
2023-08-04 14:59:02 +0800

    no comments

--- test_streamlit.py
+++ test_streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-fileName = "/Users/fenghua.gao/temp/generated_files/output_table.csv"
-df = pd.read_csv(fileName,index_col=0)
+#fileName = "/Users/fenghua.gao/temp/generated_files/output_table.csv"
+#df = pd.read_csv(fileName,index_col=0)
 
 st.write("Fenghua's test for streamlit")
 st.title('My Title')
@@ -13,5 +13,5 @@ if strinput == 'mytest':
 else:
     st.write('NO')
 
-st.dataframe(df)
+#st.dataframe(df)
 print("OK..",strinput)
